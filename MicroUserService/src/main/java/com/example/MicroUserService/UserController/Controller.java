eb3b77fde0197cfdc6fe08c6da9236f992b5a957
Prakhar8958 <pks8958saxena@gmail.com>
2024-04-22 01:04:20 +0530

    CircuitBreaker

--- MicroUserService/src/main/java/com/example/MicroUserService/UserController/Controller.java
+++ MicroUserService/src/main/java/com/example/MicroUserService/UserController/Controller.java
@@ -2,6 +2,7 @@ package com.example.MicroUserService.UserController;
 
 import com.example.MicroUserService.UserEntity.User;
 import com.example.MicroUserService.UserService.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,11 +23,19 @@ public class Controller {
     }
 
     @GetMapping("/getUser/{userid}")
+    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallBack")
     public ResponseEntity<User> getUserById(@PathVariable String userid){
         User user=userService.getUser(userid);
         return ResponseEntity.ok(user);
     }
 
+    public ResponseEntity<User> ratingHotelFallBack(String userId,Exception ex){
+        System.out.println(ex.getMessage());
+        User user=User.builder()
+                .email("pks8958saxena@gmail.com")
+                .about("dummy user created").name("Prakhar").build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
     @GetMapping("/getAllUser")
     public ResponseEntity<List<User>> getAllUser(){
         List<User> allUser=userService.getAllUser();
